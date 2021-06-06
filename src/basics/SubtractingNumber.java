5ca81c8817cf5a8d237f882c38a767ae80d2348e
Mohammed Younus <tawaabyounus1999@gmail.com>
2021-06-06 19:09:44 +0530

    in subtract sum added

--- src/basics/SubtractingNumber.java
+++ src/basics/SubtractingNumber.java
@@ -5,6 +5,7 @@ public class SubtractingNumber {
 	public static void main(String[] args) {
 		int num1 = 20;
 		int num2 = 10;
-		System.out.println(num1-num2);
+		int sum = num1-num2;
+		System.out.println(sum);
 	}
 }
